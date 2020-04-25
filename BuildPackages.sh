e0007535d20f8edddfec4e833518ec40f2d7131f
xxthepersonx <zackstephens54@gmail.com>
2020-04-25 14:49:39 -0400

    Trying out new Repo template

--- BuildPackages.sh
+++ BuildPackages.sh
@@ -6,6 +6,8 @@ gzip -cf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
 
+find . -name '.DS_Store' -type f -delete
+
 git add -A
 git commit -m "Trying out new Repo template"
 git push -u origin master
