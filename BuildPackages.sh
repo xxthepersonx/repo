c6b5b62d0eb6f6a0838ddb911711143ccd79c98b
xxthepersonx <zackstephens54@gmail.com>
2020-04-25 15:43:33 -0400

    updated some packages!

--- BuildPackages.sh
+++ BuildPackages.sh
@@ -9,5 +9,5 @@ bzip2 -cf Packages > Packages.bz2
 find . -name '.DS_Store' -type f -delete
 
 git add -A
-git commit -m "Building some updates"
+git commit -m "updated some packages!"
 git push -u origin master
