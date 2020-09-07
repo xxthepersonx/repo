c408e9383f1400e74d1041cd0dc9911725469934
xxthepersonx <zackstephens54@gmail.com>
2020-09-06 22:26:15 -0400

    Added MovePageDots!

--- BuildPackages.sh
+++ BuildPackages.sh
@@ -9,5 +9,5 @@ bzip2 -cf Packages > Packages.bz2
 find . -name '.DS_Store' -type f -delete
 
 git add -A
-git commit -m "removed pesky DS_Store!"
+git commit -m "Added MovePageDots!"
 git push -u origin master
